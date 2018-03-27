94127bea2734ad40ba68ea98e90bca45e5d09ae5
Kyle Hughes <kylehughes1@gmail.com>
2018-03-27 11:34:00 -0500

    renamed copied class names

--- letter_loop.py
+++ letter_loop.py
@@ -11,8 +11,8 @@ from random import choice
 class LetterGame:
     def __init__(self):
         self.alphabet = [
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
             ]
         self.used = []
 
--- main.py
+++ main.py
@@ -3,21 +3,28 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from threading import Thread
 from letter_loop import *
+
 
 class KidsGame(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        game = LetterGame()
-        self.ids.current.text = ''
+        self.loop = Thread(target=self.play)
+        self.loop.start()
 
     def play(self):
-        for letter in game.loop():
-            self.ids.current.text = letter
+        game = LetterGame()
+        while True:
+            letter = game.challenge()
+            self.ids.challenge.text = letter.upper()
+            if not game.attempt(letter):
+                break
 
 
 class KidsGameApp(App):
     pass
+
 
 if __name__ == '__main__':
     KidsGameApp().run()
